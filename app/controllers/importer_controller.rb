e0428b2157965a0126cfb0d270830b93a8dd7758
Adrien Bouchoux <adrien.bouchoux@ensimag.grenoble-inp.fr>
2016-05-24 16:25:16 +0200

    Lecture du token dans l'url

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -2,7 +2,7 @@ class ImporterController < ApplicationController
   protect_from_forgery with: :null_session
   HMAC_SECRET = 'secret_key'
   def index
-    decoded_token = JWT.decode token, ImporterController::HMAC_SECRET, :algorithm => 'HS256'
+    decoded_token = JWT.decode params[:token], ImporterController::HMAC_SECRET, true, { :algorithm => 'HS256' }
     data = decoded_token.first
 
     render :json => data

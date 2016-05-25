bb3ae9ff460e615c348a756ebf3b94988523fa0a
Adrien Bouchoux <adrien.bouchoux@ensimag.grenoble-inp.fr>
2016-05-25 17:26:18 +0200

    Gestion de l'api key dans les fichiers de config

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -1,8 +1,9 @@
 class ImporterController < ApplicationController
   protect_from_forgery with: :null_session
-  HMAC_SECRET = 'secret_key'
   def index
-    decoded_token = JWT.decode params[:data], ImporterController::HMAC_SECRET, true, { :algorithm => 'HS256' }
+    byebug
+    api_key = Rails.configuration.x.api['api_key']
+    decoded_token = JWT.decode params[:data], api_key, true, :algorithm => 'HS256'
     data_json = decoded_token.first['data']
     data = ActiveSupport::JSON.decode(data_json);
     #render :json => data
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Radiocampus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.x.api = config_for(:api)
   end
 end

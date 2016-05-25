63314ca7597d17f44aad1a33b100df6bd57b6672
Adrien Bouchoux <adrien.bouchoux@ensimag.grenoble-inp.fr>
2016-05-25 12:07:52 +0200

    Lecture de JWT avec les pistes

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -2,8 +2,9 @@ class ImporterController < ApplicationController
   protect_from_forgery with: :null_session
   HMAC_SECRET = 'secret_key'
   def index
-    decoded_token = JWT.decode params[:token], ImporterController::HMAC_SECRET, true, { :algorithm => 'HS256' }
-    data = decoded_token.first
+    decoded_token = JWT.decode params[:data], ImporterController::HMAC_SECRET, true, { :algorithm => 'HS256' }
+    data_json = decoded_token.first['data']
+    data = ActiveSupport::JSON.decode(data_json);
 
     render :json => data
   end

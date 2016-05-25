7f4bac3936653b31f4e395e0127f855f06fe7db2
Adrien Bouchoux <adrien.bouchoux@ensimag.grenoble-inp.fr>
2016-05-25 18:44:36 +0200

    Ebauche contrôleur rapport

--- app/controllers/rapport_controller.rb
+++ app/controllers/rapport_controller.rb
@@ -1,6 +1,25 @@
 class RapportController < ApplicationController
   def index
-    data = []
-    render :json => data
+    begin_date = params[:begin_date]
+    end_date = params[:end_date]
+    styles = params[:styles]
+
+    # effectue le mapping entre les styles Disco et Rivendell
+    styles.map! { |style| disco_to_rivendell style }
+
+    if valid_date?(begin_date) && valid_date?(end_date)
+      render :json => []
+    else
+      render :json => { :error => 'invalid date' }
+    end
+
   end
+
+  private
+
+  def valid_date?(date_string)
+    d, m, y = date_string.split '/'
+    Date.valid_date? y.to_i, m.to_i, d.to_i
+  end
+
 end

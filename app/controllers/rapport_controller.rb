82923ba191d26c99c62de9f849deaff15dbacb4e
Adrien Bouchoux <adrien.bouchoux@ensimag.grenoble-inp.fr>
2016-05-26 17:17:38 +0200

    Gestion d'un rapport sur une seule date

--- app/controllers/rapport_controller.rb
+++ app/controllers/rapport_controller.rb
@@ -2,13 +2,33 @@ class RapportController < ApplicationController
   def index
     begin_date = params[:begin_date]
     end_date = params[:end_date]
-    styles = params[:styles]
+    #styles_disco = params[:styles]
+    styles_disco = [ 164, 4]  
+
+    counters = []
+    styles_disco.each { counters << 0 }
 
     # effectue le mapping entre les styles Disco et Rivendell
-    styles.map! { |style| disco_to_rivendell style }
+    styles_rivendell = styles_disco.map { |style| disco_to_rivendell style }
 
     if valid_date?(begin_date) && valid_date?(end_date)
-      render :json => []
+      if begin_date != end_date
+        render :json => { :error => 'unsupported' }
+      else
+        con = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "radiocampus")
+        con.select_db "Rivendell"
+        formatted_date = format_date begin_date
+        styles_rivendell.each_with_index do |style, i|
+          # sur la table, si GRACE_TIME = 0 alors c'est une musique et pas une émission
+          query = "SELECT COUNT(l.ID) AS count FROM #{formatted_date}_LOG l, CART c
+          WHERE GRACE_TIME = 0 AND CART_NUMBER = c.NUMBER AND c.SCHED_CODES LIKE '%#{style}%'"
+          rs = con.query(query)
+          counters[i] += rs.first['count']
+        end
+        con.close
+
+        render :json => result_hash(styles_disco, counters)
+      end
     else
       render :json => { :error => 'invalid date' }
     end
@@ -20,6 +40,16 @@ class RapportController < ApplicationController
   def valid_date?(date_string)
     d, m, y = date_string.split '/'
     Date.valid_date? y.to_i, m.to_i, d.to_i
+  end
+
+  def format_date(date_string)
+    d, m, y = date_string.split '/'
+    "#{y}_#{m}_#{d}"
+  end
+
+  def result_hash(keys, values)
+    zipped = keys.zip(values)
+    Hash[zipped]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'importer' => 'importer#index'
   post 'importer' => 'importer#index'
+
+
+  get 'rapport' => 'rapport#index'
+  post 'rapport' => 'rapport#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

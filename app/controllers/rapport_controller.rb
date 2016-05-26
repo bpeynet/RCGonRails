dc699f725cd09e5609883f5609e6c1a66dd75ffa
LeVu <louiskim.levu@gmail.com>
2016-05-26 18:55:56 +0200

    gestion rapporst sur plusieurs jours

--- app/controllers/rapport_controller.rb
+++ app/controllers/rapport_controller.rb
@@ -2,7 +2,7 @@ class RapportController < ApplicationController
   def index
     begin_date = params[:begin_date]
     end_date = params[:end_date]
-
+    
     #styles_disco = params[:styles]
     styles_disco = [ 164, 4]  
 
@@ -14,47 +14,78 @@ class RapportController < ApplicationController
 
 
     if valid_date?(begin_date) && valid_date?(end_date)
-      if begin_date != end_date
-        render :json => { :error => 'unsupported' }
-      else
-
-        con = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "radiocampus")
-        con.select_db "Rivendell"
-        formatted_date = format_date begin_date
-        styles_rivendell.each_with_index do |style, i|
-          # sur la table, si GRACE_TIME = 0 alors c'est une musique et pas une émission
-          query = "SELECT COUNT(l.ID) AS count FROM #{formatted_date}_LOG l, CART c
+    #création de connection avec la db Rivendell
+         con = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "radiocampus")
+         con.select_db "Rivendell"
+         
+         #pour les requetes sql 
+         formatted_date_begin = format_date begin_date
+         formatted_date_end = format_date end_date
+         formatted_date_current = formatted_date_begin
+         
+         # pour les conditions ruby
+         to_date_begin = to_date begin_date
+         to_date_end = to_date end_date
+         to_date_current = to_date_begin
+         
+         # pour pqsser du format Date au string formatted
+      
+      # cas général
+      #render :json => { :error => 'unsupported' }
+       
+       while to_date_current <= to_date_end do
+       
+       styles_rivendell.each_with_index do |style,j|
+        reverse_date_current = reverse_date to_date_current
+       query = "SELECT COUNT(l.ID) AS count FROM #{reverse_date_current}_LOG l, CART c
           WHERE GRACE_TIME = 0 AND CART_NUMBER = c.NUMBER AND c.SCHED_CODES LIKE '%#{style}%'"
           rs = con.query(query)
-          counters[i] += rs.first['count']
-        end
+          counters[j] += rs.first['count']
+          
+          to_date_current=to_date_current +1.day 
+          # a priori on peut faire comme ca mais a voir comment utiliser .tomorrow
+          end
+          end
         con.close
 
         render :json => result_hash(styles_disco, counters)
 
-      end
-    else
+     
+    else   
+    # cas impossible mais bon, il fallait le faire 
+
       render :json => { :error => 'invalid date' }
     end
-​
+
   end
-​
+
   private
-​
+
   def valid_date?(date_string)
     d, m, y = date_string.split '/'
     Date.valid_date? y.to_i, m.to_i, d.to_i
   end
-
-
+  
+  def to_date(date_string)
+   d, m, y = date_string.split '/'
+   Date.new(y.to_i, m.to_i, d.to_i)
+  end
+  
   def format_date(date_string)
     d, m, y = date_string.split '/'
     "#{y}_#{m}_#{d}"
   end
-
+  
   def result_hash(keys, values)
     zipped = keys.zip(values)
     Hash[zipped]
   end
-
+  
+  def reverse_date(date)
+  old =date.to_s
+  y,m,d= old.split '-'
+  
+  "#{y}_#{m}_#{d}"
+  
+  end
 end

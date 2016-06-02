7b49903fe814cd5d1527a6379b39ca8b61a14d66
Benoît <benoit.peynet@phelma.grenoble-inp.fr>
2016-06-02 16:19:53 +0200

    Création de cartouche dans Rivendell OK

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -6,6 +6,10 @@ class ImporterController < ApplicationController
   before_action :check_authorization_upload, :only => :upload
   
   def index
+    b = Rivendell::Import::Base.new
+    f = Rivendell::Import::File.new "/home/ben/Bureau/Vanina.mp3"
+    byebug
+    t = b.create_task f
     render text: "signature verified"
     #DatabaseConnection::connect do |conn|
       #rs = conn.query("Select title from CART where number = 210660")
@@ -17,11 +21,40 @@ class ImporterController < ApplicationController
 
   def upload
     render :json => params
-    f = Rivendell::Import::File.new "BP"
+    conf = Rivendell::Import::Config.new
+    conf.rivendell.host = "192.168.1.10"
+    conf.rivendell.db_url = "mysql://rduser:letmein@192.168.1.10/Rivendell"
+    file = params['multifile_0']
+    f = Rivendell::Import::File.new file.path
     t = Rivendell::Import::Task.new :file => f
     c = Rivendell::Import::Cart.new t
     c.group = "MUSIC"
+    data = ActiveSupport::JSON.decode params['data']
+    byebug
+
     c.create
+    c.cut.create
+    c.import f
+
+    c.title = data['titre']
+    c.artist = data['artiste']
+    c.album = data['album']
+    data['styles'].each do |style_disco|
+      style_rivendell = mapping_hash[style_disco]
+      unless style_rivendell.nil?
+	c.scheduler_codes << style_rivendell
+      end
+    end
+    style_rivendell = mapping_hash[data['genre']]
+    unless style_rivendell.nil?
+      c.scheduler_codes << style_rivendell
+    end
+    c.scheduler_codes << "MuPlayList"
+    if data['fr'] 
+      c.scheduler_codes << "MuFR"
+    end
+    c.update
+
   end
 
   private
@@ -41,8 +74,8 @@ class ImporterController < ApplicationController
 
   def check_authorization_upload
     file = params['multifile_0']
-    if file && signature_verified?(file.read)
-      return
+    if !file.nil? && signature_verified?(file.read)
+      return true
     end
     head :forbidden
     return false

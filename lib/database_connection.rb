ffebdd8c7fdc8dbc839b5f54cc496c51f0d558cf
Benoît <benoit.peynet@phelma.grenoble-inp.fr>
2016-06-08 17:17:34 +0200

    Ajout de paramètres pour installations

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,6 +1,6 @@
 module DatabaseConnection
   def self.connect
-    conn = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "radiocampus")
+    conn = Mysql2::Client.new(:host => Rails.configuration.x.api['rivendell_host'], :username => Rails.configuration.x.api['rivendell_user'], :password => Rails.configuration.x.api['rivendell_password'])
     conn.select_db "Rivendell"
     yield conn
     conn.close
